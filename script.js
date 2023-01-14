d5264fc8d4eb8152a190c1d83240db69591315c1
johaj10 <78065218+johaj10@users.noreply.github.com>
2023-01-13 23:48:01 -0800

    Add selector

--- script.js
+++ script.js
@@ -3,6 +3,41 @@ const ctx = canvas.getContext('2d');
 
 const degToRad = (degrees) => (Math.PI/180) * degrees;
 
+
+
+let circleDrawn = false;
+let oldX, oldY;
+
+function mousePosition(event) {
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+
+    console.log("Coordinate x: " + x, "Coordinate y: " + y);
+
+    if (!circleDrawn) {
+        ctx.beginPath();
+        ctx.arc(x, y, 8, 0, Math.PI*2);
+        ctx.stroke();
+        
+    } else {
+        ctx.clearRect(oldX, oldY, 8, 8);
+        ctx.beginPath();
+        ctx.arc(x, y, 8, 0, Math.PI*2);
+        ctx.stroke();
+    }
+
+    oldX = x;
+    oldY = y;
+    circleDrawn = true;
+}
+
+
+canvas.addEventListener("mousedown", (e) => {
+    mousePosition(e);
+});
+
+
 /*
 ctx.fillStyle = 'red';
 ctx.fillRect(20, 20, 300, 100);
@@ -29,21 +64,3 @@ ctx.beginPath();
 ctx.arc(300, 300, 20, 0, Math.PI*2);
 ctx.stroke();
 */
-
-function mousePosition(event) {
-    let rect = canvas.getBoundingClientRect();
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-
-    console.log("Coordinate x: " + x, "Coordinate y: " + y);
-
-    ctx.beginPath();
-    ctx.arc(x, y, 8, 0, Math.PI*2);
-    ctx.stroke();
-
-}
-
-
-canvas.addEventListener("mousedown", (e) => {
-    mousePosition(e);
-});
